c19dc78f23db9dec1093f24cadba64e0269f6b8e
Phil <kimathi.phil@gmail.com>
2018-03-14 13:15:53 +0300

    created gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+	console.log("Hooray! You created a gulp task.");
+});
+
+gulp.task('html', function() {
+	console.log("Imagine something useful being done to your HTML here.");
+});
