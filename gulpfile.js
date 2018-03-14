c0ec9b822bc6308238aa30f2c2e836f882822a16
Phil <kimathi.phil@gmail.com>
2018-03-14 13:53:19 +0300

    Added watch task for index.html and styles.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch');
 
 gulp.task('default', function() {
 	console.log("Hooray! You created a gulp task.");
@@ -6,4 +7,20 @@ gulp.task('default', function() {
 
 gulp.task('html', function() {
 	console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('styles', function() {
+	console.log("Imagine Sass or PostCSS tasks running here.");
+});
+
+gulp.task('watch', function() {
+
+	watch('./app/index.html', function() {
+		gulp.start('html'); 
+	});
+
+	watch('./app/assets/styles/**/*styles.css', function() {
+	gulp.start('styles');
+	});
+
 });
